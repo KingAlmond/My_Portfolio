b3f61e208d65b8cc533e4027a7b4e3e5cfd8032e
KingAlmond <adesai2018@gmail.com>
2019-02-24 03:33:26 -0800

    menu overlay, bug fixes

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,7 +3,7 @@ const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
-const navItem = document.querySelectorAll("nav-item");
+const navItems = document.querySelectorAll(".nav-item");
 
 // Set intial state of the menu , overlay
 let showMenu = false;
@@ -16,16 +16,15 @@ function toggleMenu() {
     menu.classList.add("show");
     menuNav.classList.add("show");
     menuBranding.classList.add("show");
-    navItem.forEach(item => item.classList.add("show"));
+    navItems.forEach(item => item.classList.add("show"));
 
-    //reset menu state
     showMenu = true;
   } else {
     menuBtn.classList.remove("close");
     menu.classList.remove("show");
     menuNav.classList.remove("show");
     menuBranding.classList.remove("show");
-    navItem.forEach(item => item.classList.remove("show"));
+    navItems.forEach(item => item.classList.remove("show"));
 
     //reset menu state
     showMenu = false;
